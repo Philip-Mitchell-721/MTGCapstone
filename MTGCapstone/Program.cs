6164c30922d4104ef2755581d97c6f7f477d84d9
Philip Mitchell <122426528+Philip-Mitchell-721@users.noreply.github.com>
2023-02-23 12:49:26 -0500

    updating location (repo to github folder)

--- MTGCapstone/Program.cs
+++ MTGCapstone/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//comment in main
 builder.Services.AddControllers()
     .AddNewtonsoftJson(); 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
